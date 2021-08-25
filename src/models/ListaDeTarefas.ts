9f063a4d99a461c17617b011202b47934564a92b
Victor Nasser <63263710+VitaoNasser@users.noreply.github.com>
2021-08-25 12:03:34 -0300

    second commit

--- src/models/ListaDeTarefas.ts
+++ src/models/ListaDeTarefas.ts
@@ -8,10 +8,27 @@ export class ListaDeTarefas{
     tabela:HTMLTableElement;
     
     constructor(main:HTMLElement){
+        
         this.input  = <HTMLInputElement>main.querySelector("#tf_2do");
         this.form   = <HTMLFormElement>main.querySelector("#form");
         this.tabela = <HTMLTableElement>main.querySelector("#table");
-        this.tarefas = [];
+
+        // Tentando carregar dados do localStorage
+        let dados:string = window.localStorage.getItem("todolist");
+        if(dados == null){
+            window.localStorage.setItem("todolist","[]");
+            this.tarefas = [];
+        } else {
+            this.tarefas = <Tarefa[]>JSON.parse(dados).map(
+                t => {
+                    let novaTarefa = new Tarefa(t.texto, t.prioridade);
+                    novaTarefa.id = t.id;
+                    return novaTarefa;
+                }
+            );
+        };
+
+        this.mostrarTarefas();
         
         //Quando o form for submetido, que se adicione uma tarefa
         this.form.addEventListener("submit", (event) => {
@@ -22,15 +39,14 @@ export class ListaDeTarefas{
     }
     
     removerTarefa(t:Tarefa){
-        this.tarefas.splice(this.tarefas.indexOf(t),1);
+        this.tarefas.splice(this.tarefas.indexOf(t),1);        
         document.getElementById(t.id).remove();
-
     }
+    
     adicionarTarefa(){
 
         // Verificar se o input tem alguma string!
         if(this.input.value == "") return;
-
 
         // Criar nova tarefa com prioridade baixa e com texto digitado pelo usuário
         let t = new Tarefa(this.input.value, Prioridade.Baixa);
@@ -38,12 +54,14 @@ export class ListaDeTarefas{
         // Adicionar a tarefa criada a array tarefas
         this.tarefas.push(t);
 
+        // salvar o array de tarefas no localStorage
+        window.localStorage.setItem("todolist",JSON.stringify(this.tarefas));
+
         // Criando a linha de tarefa
         let tr = t.toRow();
 
         tr.querySelector("i").addEventListener("click",
         () => {
-            tr.remove();
             this.removerTarefa(t);
             }
         );
@@ -57,11 +75,17 @@ export class ListaDeTarefas{
     }
 
     mostrarTarefas():void {
-        // Limpar tabela
         this.tabela.innerHTML = "";
-        // Mostrar tarefas no html
-        for(let tarefa of this.tarefas){
-            this.tabela.appendChild(tarefa.toRow());
-        }
+        this.tarefas.forEach(
+            t => {
+                let tr = t.toRow();
+                tr.querySelector("i").addEventListener("click",
+                    () => {
+                        this.removerTarefa(t);
+                    }
+            );
+            this.tabela.appendChild(tr);
+            }
+        )
     }
 }
